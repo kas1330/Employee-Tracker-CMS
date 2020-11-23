c052c62c4449e687a8e1ed70b942a5ab9ea8af4e
Kerri Sexton <k.sexton804@gmail.com>
2020-11-22 20:32:42 -0500

    boilerplate code to get the js file connected to the db

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,2 +1,22 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "mysql23@#",
+    database: "employeeDB"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
